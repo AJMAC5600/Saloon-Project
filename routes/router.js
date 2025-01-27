19297b2ded116c9ca1455acfdfbd8cd12786f229
Abhishek Jha <ajmac5600@gmail.com>
2025-01-27 15:25:11 +0530

    commit

--- routes/router.js
+++ routes/router.js
@@ -6,8 +6,16 @@ router.get('/', (req, res) => {
   res.render('pages/Home'); // Render the Home.ejs view
 });
 
-router.get('/about', (req, res) => {
-  res.send('About Page');
+router.get('/About', (req, res) => {
+  res.render('pages/About'); // Render the About.ejs view
+});
+
+router.get('/Contact', (req, res) => {
+  res.render('pages/Contact'); // Render the Contact.ejs view
+});
+
+router.get('/Typography', (req, res) => {
+  res.render('pages/Typography'); // Render the Typography.ejs view
 });
 
 module.exports = router;
